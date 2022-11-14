60ccf55ef044ec435f44571dbaacc3a9a91c2d69
Filip <filipkraehenbuehl@gmail.com>
2022-11-14 12:19:23 +0100

    added files, imported files, merged files, filtered out averages using pandas
    Add files via upload
    
    added gitnore, readme
    
    cleaned up

---
+++ README.md.txt
@@ -0,0 +1,15 @@
+# Diabetes Technologies Meal Detection
+
+**Attention!** DMMSR_Dataset exceeds github-filesize limitations, therefore download dataset individually
+
+Expected filestructure:
+
+.idea
+DMMSR_Dataset
+	DMMSR_Dataset
+		adolescents
+		adults
+		children
+venv
+main.py
+readme.py
--- main.py
+++ main.py
@@ -1,16 +1,44 @@
+import csv
+import glob, os
+import pandas as pd
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+#PATH_TO_DMMSR_DATASET=".\DMMSR_Dataset\DMMSR_Dataset"
+PROJECT_HOMEFOLDER=os.getcwd()
+PATH_TO_DMMSR_DATASET=PROJECT_HOMEFOLDER+".\DMMSR_Dataset\DMMSR_Dataset"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    #merge all excel into a dataframe, if it doesnt exist yet
+    isFile = os.path.isfile(PATH_TO_DMMSR_DATASET+"/merged.csv")
+    if not isFile:
+        df_tot=pd.DataFrame()
+        os.chdir(PATH_TO_DMMSR_DATASET+"/adolecents")
+        for file in glob.glob("*.csv"):
+            df = pd.read_csv(file)
+            df_tot=df_tot.append(df,ignore_index=True)
+
+        os.chdir(PATH_TO_DMMSR_DATASET+"/adults")
+        for file in glob.glob("*.csv"):
+            df = pd.read_csv(file)
+            df_tot=df_tot.append(df,ignore_index=True)
+
+        os.chdir(PATH_TO_DMMSR_DATASET + "/children")
+        for file in glob.glob("*.csv"):
+            df = pd.read_csv(file)
+            df_tot=df_tot.append(df,ignore_index=True)
+        df_tot.to_csv(PATH_TO_DMMSR_DATASET+'/merged.csv',index=False)
+        print("merged all files together")
+    else:
+        df_tot=pd.read_csv(PATH_TO_DMMSR_DATASET+'/merged.csv')
+
+    #filter out #average
+    df_tot = df_tot[
+        (df_tot['name'] != 'adolescent#average') & (df_tot['name'] != 'adult#average') & (df_tot['name'] != 'child#average')]
+    pass
+    #binar<y labeling
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
