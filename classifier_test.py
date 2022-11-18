8ded092412e5a9513a5f451c49cb93e3054bd553
VincentIDK <115032499+VincentIDK@users.noreply.github.com>
2022-11-18 18:43:56 +0100

    Add files via upload

--- classifier_test.py
+++ classifier_test.py
@@ -11,13 +11,30 @@ import os
 import torch as th
 import pandas as pd
 
+#data handling
 
 dir = "C:/Users/vince/OneDrive/Unibe/Semester_3/diabetes management/project/DMMSR_Dataset/DMMSR_Dataset/adolecents"
-data = pd.read_csv(os.path.join(dir, "adolescent#001.csv"))
-#data = pd.read.csv(C:/Users/vince/OneDrive/Unibe/Semester_3/diabetes management/project")
-insulin = data["sqInsulinNormalBolus"]
-insulin = insulin.to_numpy()
-time = data["minutesPastSimStart"]
-time = time.to_numpy()
-cgm = data["cgm"]
-cgm = cgm.to_numpy()
+file = pd.read_csv(os.path.join(dir, "adolescent#001.csv"))
+insulin = file["sqInsulinNormalBolus"].to_numpy()
+time = file["minutesPastSimStart"].to_numpy()
+cgm = file["cgm"].to_numpy()
+
+interval = 15 #choose an intervaöö of 15 minutes
+length = int(np.floor(np.size(time)/interval)) 
+
+labels = []
+
+for i in range(0,length):
+     labels.append(any(insulin[i*15:i*15+15]!=0))
+
+labels = (np.asarray(labels)).astype(float) #2976x1 float array
+
+data = []
+
+for i in range(0,length):
+    data.append(cgm[i*15:i*15+15])
+    
+data = np.asarray(data) #2976x15 float array
+
+#classifier
+
