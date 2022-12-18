6448c79c7520b83fdd373d60fb90f971ee79f100
VincentIDK <115032499+VincentIDK@users.noreply.github.com>
2022-12-18 20:01:18 +0100

    Add files via upload

--- LSTMmodel.py
+++ LSTMmodel.py
@@ -9,7 +9,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from numpy import array
 import os
-import torch as th
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -18,6 +17,7 @@ import tensorflow as tf
 
 from keras import backend as K
 
+#functions
 def recall_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
@@ -46,7 +46,6 @@ def split_data(cgm,insulin, n_steps):
  # check if we are beyond the sequence
      if end_ix > len(cgm)-1:
         break
- # gather input and output parts of the pattern
      offset = 15
      i_off = i+offset
      end_ix_off = end_ix+offset
@@ -59,17 +58,20 @@ def split_data(cgm,insulin, n_steps):
 
  
  # read data
-dir = "C:/Users/vince/OneDrive/Unibe/Semester_3/diabetes management/project/DMMSR_Dataset/DMMSR_Dataset/children"
-file = pd.read_csv(os.path.join(dir, "child#006.csv"))
+dir = "C:/Users/vince/OneDrive/Unibe/Semester_3/diabetes management/project/DMMSR_Dataset/DMMSR_Dataset/adolecents"
+file = pd.read_csv(os.path.join(dir, "adolescent#005.csv"))
 insulin = file["sqInsulinNormalBolus"].to_numpy()
 time = file["minutesPastSimStart"].to_numpy()
 cgm = file["cgm"].to_numpy()
 cho = file["CR"].to_numpy()
- 
+
+
+#create samples 
 series_length = 30
- 
 data, labels = split_data(cgm,insulin,series_length)
 labels = labels.astype(int)
+
+ #normalization
  
 for row in range(0,len(data)):
     mue = np.mean(data[row])
@@ -77,9 +79,11 @@ for row in range(0,len(data)):
     data[row] = (data[row]-mue)/sigma
      
 
+#create sets
  
 data_train = data[0:int(0.8*len(data))]
 labels_train = labels[0:int(0.8*len(data))]
+
  
 data_val = data[int(0.8*len(data)):int(0.9*len(data))]
 labels_val = labels[int(0.8*len(data)):int(0.9*len(data))]
@@ -87,7 +91,6 @@ labels_val = labels[int(0.8*len(data)):int(0.9*len(data))]
 data_test = data[int(0.9*len(data)):-1]
 labels_test = labels[int(0.9*len(data)):-1]
  
- #normalization
 
  
 
@@ -97,37 +100,37 @@ n_features = 1
 model = Sequential()
 model.add(LSTM(25, activation='relu', input_shape=(n_steps, n_features)))
 model.add(Dense(units=128,activation='relu'))
-#model.add(Dense(1, activation='sigmoid'))
 model.add(Dense(1, activation='sigmoid'))
 
-#model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc',f1_m,precision_m, recall_m])
-#print(model.summary())
-#print("test")
 
 
+#minoritiy oversampling
 from imblearn.over_sampling import SMOTE
-#sm = SMOTE(random_state=0)
 sm = SMOTE( sampling_strategy=1, random_state=None, k_neighbors=5, n_jobs=None)
 x_resampled, y_resampled = sm.fit_resample(data_train, labels_train)
 
+#training
+model.fit(x_resampled,y_resampled,batch_size=16, epochs=10)
+#model.fit(data_train,labels_train,batch_size=16, epochs=1)
 
-
-model.fit(x_resampled,y_resampled,batch_size=16, epochs=20)
-#model.fit(x_resampled,y_resampled, epochs=100)
-
+#prediction
 predict = model.predict(data_val)
+predict_test = model.predict(data_test)
 
 
 
 
+#evaluation
 from sklearn.metrics import roc_curve
 
 fpr, tpr,tresholds = roc_curve(labels_val,predict) #fpr = false positive, trp = true positive
 #select threshold from validation set and apply to test set (10% val set, train 70%, rest test)
+fpr_t, tpr_t,tresholds_t = roc_curve(labels_test,predict_test)
 
 plt.subplot(1,2,1)
-plt.plot(fpr,tpr)
+plt.plot(fpr,tpr,label="validation")
+plt.plot(fpr_t,tpr_t,'r',label="testing")
 plt.title("ROC_curve")
 plt.xlabel("specifity")
 plt.ylabel("sensitivity")
@@ -136,11 +139,15 @@ plt.grid()
 from sklearn.metrics import precision_recall_curve
 
 prec,rec,tresholds2 = precision_recall_curve(labels_val,predict)
+prec_t,rec_t,tresholds2_t = precision_recall_curve(labels_test,predict_test)
+
 plt.subplot(1,2,2)
-plt.plot(rec,prec)
+plt.plot(rec,prec,label="validation")
+plt.plot(rec_t,prec_t,'r',label="testing")
 plt.title("precision-recall")
 plt.xlabel("Recall")
 plt.ylabel("Precision")
+plt.legend()
 plt.grid()
 
 from sklearn.metrics import roc_auc_score
